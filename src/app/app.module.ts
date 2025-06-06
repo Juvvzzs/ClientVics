2a8c7bc7e19318f471fb4130104a9fb38f5f0737
Juvvzzs <leprozojuvanie@gmail.com>
2025-06-07 05:46:43 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './vics/vics_layout/navbar/navbar.component';
+import { SidebarComponent } from './vics/vics_layout/sidebar/sidebar.component';
+import { MainlayoutComponent } from './vics/vics_layout/mainlayout/mainlayout.component';
+import { FooterComponent } from './vics/vics_layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    MainlayoutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
